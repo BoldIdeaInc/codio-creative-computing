820c163c0e9ea588eb6696841a3b26b2fe93af5c
Ben Davis <ben@boldidea.org>
2021-09-07 16:23:15 +0000

    Added custom js to disable next button after click (for slow connections)

--- .guides/guide.js
+++ .guides/guide.js
@@ -1,13 +1,43 @@
-/*
-const commands = {
-  openTutorials: function(event) {
-    codio.open('preview', 'http://scratch.mit.edu/studios/475476', 1);
+// observe innerHTML changed on element
+function onInnerHTMLChanged(selector, callback, initial=false) {
+  const observers = [];
+  for (const target of document.querySelectorAll(selector)) {
+    const observer = new MutationObserver(mutationList => {
+      for (const mutation of mutationList) {
+        callback(target);
+      }
+    });
+    var config = {
+      characterData: false,
+      attributes: false,
+      childList: true,
+      subtree: false 
+    };
+    observer.observe(target, config);
+    observers.push(observer);
+    if (initial) {
+      callback(target);
+    }
   }
 }
-window.addEventListener('codio-button-custom', function (event) {
-  console.log("EVENT", event)
-  if(codio) {
-    commands[event.cmd](event);
+
+// Disable next button when clicked
+const btn = document.querySelector('button.codio-button-type-next');
+btn.addEventListener('click', event => {
+  if (btn.getAttribute('disabled')) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  } else {
+    btn.setAttribute('disabled', 'disabled');
+    btn.setAttribute('aria-disabled', 'true');
   }
 });
-*/
+
+// Re-set next button when markdown changes
+onInnerHTMLChanged('.rendered-markdown', () => {
+  btn.removeAttribute('disabled');
+  btn.setAttribute('aria-disabled', 'false');
+});
+
+console.log('guide.js loaded');
